cdfef5093de6a4ee40106fa11f3b94893c2abe37
Brandon <brandonlovatt@gmail.com>
2021-06-21 14:00:14 -0500

    Removed "board_part_repo_paths" line in build.tcl and corrected clk_wiz ext.

--- Vivado_Project/build.tcl
+++ Vivado_Project/build.tcl
@@ -21,7 +21,6 @@ set proj_dir [get_property directory [current_project]]
 
 # Set project properties
 set obj [current_project]
-set_property -name "board_part_repo_paths" -value "[file normalize "$origin_dir/../../../../../../../AppData/Roaming/Xilinx/Vivado/2019.2/xhub/board_store"]" -objects $obj
 set_property -name "default_lib" -value "xil_defaultlib" -objects $obj
 set_property -name "enable_vhdl_2008" -value "1" -objects $obj
 set_property -name "ip_cache_permissions" -value "read write" -objects $obj

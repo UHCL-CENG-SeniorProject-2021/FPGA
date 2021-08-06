28d31aa5442cf01b62cd10909e5bc1469571c771
Brandon <brandonlovatt@gmail.com>
2021-08-06 18:02:44 -0500

    build.tcl now builds entire project. Design Sources sometimes get absorbed into Sim Sources?

--- Vivado_Project/build.tcl
+++ Vivado_Project/build.tcl
@@ -1,20 +1,10 @@
-
-# Set the reference directory for source file relative paths (by default the value is script directory path)
 set origin_dir [pwd]
-
-# Set the project name
-set _xil_proj_name_ "FPGA_Project"
-
-# Use project name variable, if specified in the tcl shell
-if { [info exists ::user_project_name] } {
-  set _xil_proj_name_ $::user_project_name
-}
-
-# Set the directory path for the original project from where this script was exported
-set orig_proj_dir "[file normalize "$origin_dir/../project_1"]"
+set prjName "FPGA_Project"
+set boardName xc7z020clg400-1
+set topName zybo_top
 
 # Create project
-create_project ${_xil_proj_name_} ./${_xil_proj_name_} -part xc7z020clg400-1
+create_project ${prjName} ./${prjName} -part xc7z020clg400-1
 
 # Set the directory path for the new project
 set proj_dir [get_property directory [current_project]]
@@ -24,22 +14,71 @@ set obj [current_project]
 set_property -name "default_lib" -value "xil_defaultlib" -objects $obj
 set_property -name "enable_vhdl_2008" -value "1" -objects $obj
 set_property -name "ip_cache_permissions" -value "read write" -objects $obj
-set_property -name "ip_output_repo" -value "$proj_dir/${_xil_proj_name_}.cache/ip" -objects $obj
+set_property -name "ip_output_repo" -value "$proj_dir/${prjName}.cache/ip" -objects $obj
 set_property -name "mem.enable_memory_map_generation" -value "1" -objects $obj
-set_property -name "part" -value "xc7z020clg400-1" -objects $obj
-set_property -name "sim.central_dir" -value "$proj_dir/${_xil_proj_name_}.ip_user_files" -objects $obj
+set_property -name "sim.central_dir" -value "$proj_dir/${prjName}.ip_user_files" -objects $obj
 set_property -name "sim.ip.auto_export_scripts" -value "1" -objects $obj
 set_property -name "simulator_language" -value "Mixed" -objects $obj
 set_property -name "target_language" -value "VHDL" -objects $obj
 set_property -name "xpm_libraries" -value "XPM_CDC" -objects $obj
 
-
-add_files [glob ${origin_dir}/*.vhd]
-add_files [glob ${origin_dir}/ip/*.xci]
-
 #
 # Sources
 #
+
+set GRLIB_SET {
+    ../Vivado_Project/grlib-main/lib/grlib/stdlib/version.vhd
+    ../Vivado_Project/grlib-main/lib/grlib/stdlib/stdlib.vhd
+    ../Vivado_Project/grlib-main/lib/grlib/stdlib/config_types.vhd
+    ../Vivado_Project/grlib-main/lib/grlib/stdlib/config.vhd
+    ../Vivado_Project/grlib-main/lib/grlib/stdlib/stdio.vhd
+    ../Vivado_Project/grlib-main/lib/grlib/stdlib/testlib.vhd
+    ../Vivado_Project/grlib-main/lib/grlib/amba/amba.vhd
+    ../Vivado_Project/grlib-main/lib/grlib/amba/devices.vhd
+    ../Vivado_Project/grlib-main/lib/grlib/amba/ahbctrl.vhd
+    ../Vivado_Project/grlib-main/lib/grlib/amba/ahbmst.vhd
+    ../Vivado_Project/grlib-main/lib/grlib/amba/apbctrl.vhd
+    ../Vivado_Project/grlib-main/lib/grlib/amba/apbctrlx.vhd
+}
+set GAISLER_SET {
+    ../Vivado_Project/grlib-main/lib/gaisler/misc/misc.vhd
+    ../Vivado_Project/grlib-main/lib/gaisler/misc/ahbram.vhd
+    ../Vivado_Project/grlib-main/lib/gaisler/misc/ahbstat.vhd
+    ../Vivado_Project/grlib-main/lib/gaisler/misc/rstgen.vhd
+    ../Vivado_Project/grlib-main/lib/gaisler/uart/uart.vhd
+	../Vivado_Project/grlib-main/lib/gaisler/uart/apbuart.vhd
+    ../Vivado_Project/grlib-main/lib/gaisler/uart/libdcom.vhd
+    ../Vivado_Project/grlib-main/lib/gaisler/uart/dcom.vhd
+    ../Vivado_Project/grlib-main/lib/gaisler/uart/dcom_uart.vhd
+    ../Vivado_Project/grlib-main/lib/gaisler/uart/ahbuart.vhd
+    ../Vivado_Project/grlib-main/lib/gaisler/leon3/leon3.vhd
+    ../Vivado_Project/grlib-main/lib/gaisler/irqmp/irqmp.vhd
+	../Vivado_Project/grlib-main/lib/gaisler/spi/spi.vhd
+	../Vivado_Project/grlib-main/lib/gaisler/spi/spictrl.vhd
+	../Vivado_Project/grlib-main/lib/gaisler/spi/spictrlx.vhd
+	../Vivado_Project/grlib-main/lib/gaisler/misc/grgpio.vhd
+}
+
+set TECHMAP_SET {
+    ../Vivado_Project/grlib-main/lib/techmap/gencomp/gencomp.vhd
+	../Vivado_Project/grlib-main/lib/techmap/gencomp/netcomp.vhd
+    ../Vivado_Project/grlib-main/lib/techmap/inferred/memory_inferred.vhd
+    ../Vivado_Project/grlib-main/lib/techmap/alltech/allmem.vhd
+    ../Vivado_Project/grlib-main/lib/techmap/maps/memrwcol.vhd
+    ../Vivado_Project/grlib-main/lib/techmap/maps/syncram.vhd
+    ../Vivado_Project/grlib-main/lib/techmap/maps/syncram_2p.vhd
+    ../Vivado_Project/grlib-main/lib/techmap/maps/syncrambw.vhd
+	../Vivado_Project/grlib-main/lib/techmap/maps/spictrl_net.vhd
+}
+
+read_vhdl -library grlib $GRLIB_SET
+read_vhdl -library techmap $TECHMAP_SET
+read_vhdl -library gaisler $GAISLER_SET
+
+#read_vhdl -library work [ glob ${origin_dir}/*.vhd]
+add_files [glob ${origin_dir}/*.vhd]
+add_files [glob ${origin_dir}/ip/*.xci]
+
 
 # Set 'sources_1' fileset properties
 set obj [get_filesets sources_1]
@@ -54,10 +93,5 @@ set obj [get_filesets constrs_1]
 
 # Add/Import constrs file and set constrs file properties
 set file "[file normalize "$origin_dir/constraints/Constraints.xdc"]"
-puts $file
 set file_imported [import_files -fileset constrs_1 [list $file]]
-puts $file_imported
 set_property -name "target_constrs_file" -value "[get_files Constraints.xdc]" -objects $obj
-
-# TODO: set up grlib/gaisler library for UART comms between fpga and ft232
-# and make necessary signal connections between components and pins

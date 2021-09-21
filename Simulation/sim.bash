41921f9b541779c437e8c349dfe62af39066d6e5
bklolo <brandonlovatt@gmail.com>
2021-09-21 00:03:05 -0500

    sim.bash test

--- Simulation/sim.bash
+++ Simulation/sim.bash
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Add cocotb_helper folder to the path
+export PYTHONPATH=$PYTHONPATH:$(pwd)/cocotb_helper/
+# Add Python folder to the path
 export PYTHONPATH=$PYTHONPATH:$(pwd)/Python/    # this adds the Python folder to the path
 cd cocotb_sim
 make

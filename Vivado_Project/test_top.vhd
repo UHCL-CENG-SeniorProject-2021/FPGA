e3bacbe8584550189026d53e634ba7427721ff70
bklolo <brandonlovatt@gmail.com>
2021-10-03 16:57:55 -0500

    previous merge broke project, updated with working version

--- Vivado_Project/test_top.vhd
+++ Vivado_Project/test_top.vhd
@@ -1,41 +1,30 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity test_top is
     port (
-----------------system signals--------------
-        iCLK: in std_logic;     -- FPGA clock
-        iRESET: in std_logic;   -- FPGA reset
+        iCLK: in std_logic;
 
         -- UART
-        iRs: in std_logic;
-        oRs: out std_logic;
-        iGPIO: out std_logic;
+        iUART: in std_logic;
+        oUART: out std_logic;
+        
         oGPIO: out std_logic
     );
 end test_top;
 
 architecture Behavioral of test_top is
 
-constant test_UART: boolean := false;
+constant test_UART: boolean := true;
 
 component logic_top is
     port (
-        iClk: in std_logic;
+        iCLK: in std_logic;
         iReset: in std_logic;
 
         -- UART
-        iRs: in std_logic;
-        oRs: out std_logic;
+        iUART: in std_logic;
+        oUART: out std_logic;
 
          --SPI
         iSck: in std_logic;
@@ -56,21 +45,21 @@ begin
 test_GRLIB: if(not test_UART)
     generate begin
 tester: logic_top port map(
-    iClk=>iClk, 
-    iReset => iRESET,
-    iUART_dbg=>iRs,
-    oUART_dbg=>oRs,
+    iCLK=>iCLK, 
+    iReset => '1',
+    iUART_dbg=>iUART,
+    oUART_dbg=>oUART,
     oGPIO=>oGPIO,
-    iRs=>'1',
+    iUART=>'1',
     iSck=>'1',
     iCsn=>'1',
     iGPIO=>(others=>'0'),
-    iMosi=>'1'    
+    iMosi=>'1'
     ); end generate;
-    
+
     UART_test: if(test_UART)
         generate begin
-        oRs<=iRs;
+        oUART<=iUART;
         oGPIO<='1';
         end generate;
 end Behavioral;
